8c533a15db21fe67da8e5dee725d7f47b887b1c6
Man9984 <70267605+Man9984@users.noreply.github.com>
2022-09-26 17:52:54 +0530

    azure_databricks

---
+++ notebooks/Users/kockpit@lbpl.co.in/Ludhiana Beverages/manjeet_ETL.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+import pandas as pd
+
+
+# COMMAND ----------
+
+for i in range(1,101):
+    print("Manjeet", end=" ")
+
+# COMMAND ----------
+
+print("Pankaj Kumar Kockpit")
+
+# COMMAND ----------
+
