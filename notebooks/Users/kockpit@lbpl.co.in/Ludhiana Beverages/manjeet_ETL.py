113f72d794a06d30611ce17d54f9529beed8c24b
Man9984 <70267605+Man9984@users.noreply.github.com>
2022-09-27 18:43:52 +0530

    Update: .gitignore LICENSE README.md notebooks

--- notebooks/Users/kockpit@lbpl.co.in/Ludhiana Beverages/manjeet_ETL.py
+++ notebooks/Users/kockpit@lbpl.co.in/Ludhiana Beverages/manjeet_ETL.py
@@ -13,3 +13,28 @@ print("Pankaj Kumar Kockpit")
 
 # COMMAND ----------
 
+l = lambda a, b : a+b
+s=l(12,1)
+print(s)
+
+# COMMAND ----------
+
+def add(a,b):
+    print("Sum : ", a+b)
+    return a+b
+
+a = add(2,3)
+print("Sum Of Two Number : ",a)
+
+# COMMAND ----------
+
+for i in range(1, 100,4):
+    print(i, end =" ")
+
+# COMMAND ----------
+
+for i in range(1,10):
+    print("LBPL" , end= " ")
+
+# COMMAND ----------
+
